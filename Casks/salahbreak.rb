9e92b271b24a88938eb7562c4b50efbf54fb688a
Mohammed Elnaggar <moelnaggar14@gmail.com>
2026-05-22 11:46:49 +0300

    salahbreak 1.0.0: re-roll DMG with Sparkle init guard (sha256 update)

--- Casks/salahbreak.rb
+++ Casks/salahbreak.rb
@@ -1,6 +1,6 @@
 cask "salahbreak" do
   version "1.0.0"
-  sha256 "128b6870a4ad64bf61f4135fa61db5abb921bba5fc67bdb595fe8f42101bc926"
+  sha256 "bfaa0a127099102fb672f711c22534db6b8c71739a84303ee7df9b3267c01365"
 
   url "https://github.com/salahbreak/salahbreak/releases/download/v#{version}/SalahBreak-#{version}.dmg",
       verified: "github.com/salahbreak/"
